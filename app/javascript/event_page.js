f683c471545bf5e566e641052658ecce87712c54
Angeliano Sousa <angeliano.sousa@gmail.com>
2025-10-04 23:15:04 -0300

    clean: Limpa arquivos

--- app/javascript/event_page.js
+++ app/javascript/event_page.js
@@ -3,12 +3,7 @@ window.$ = window.jQuery = jquery;
 
 import "bootstrap";
 import 'jquery-mask-plugin';
-// import './company_module/event_pages/form_buy_ticket';
 
 $('.phone-mask').mask('(00) 0000-0000');
 $('.cellphone-mask').mask('(00) 9 0000-0000');
 $('.cpf-mask').mask('000.000.000-00');  
-
-document.addEventListener("turbo:load", () => {
-  console.log("jQuery version:", $.fn.jquery);
-});
