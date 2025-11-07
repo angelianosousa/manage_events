d0d15a384ce5050b9cb67ae56b291b2c3b2582ad
Angeliano Sousa <angeliano.sousa@gmail.com>
2025-11-06 22:17:43 -0300

    fix: Corrige css para fixa o tamanho do banner principal do evento

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+   before_action :set_active_storage_url_options
+
+  private
+
+  def set_active_storage_url_options
+    ActiveStorage::Current.url_options = {
+      host: request.host,
+      port: request.port,
+      protocol: request.protocol
+    }
+  end
 end
