aa0fc3a094860d50a8649fc93cac41802bc8f256
Angeliano Sousa <angeliano.sousa@gmail.com>
2025-11-07 17:42:07 -0300

    fix: Corrige migrações

--- db/migrate/20251107202415_add_fields_to_payment.rb
+++
@@ -1,7 +0,0 @@
-class AddFieldsToPayment < ActiveRecord::Migration[7.1]
-  def change
-    add_column :payments, :cancelled_at, :datetime
-    add_column :payments, :expired_at, :datetime
-    add_column :payments, :paided_at, :datetime
-  end
-end
